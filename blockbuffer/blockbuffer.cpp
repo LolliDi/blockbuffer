51848e0b46962624cf3a4490d365190b301c49cd
Lolli Di <290romchik@gmail.com>
2021-12-04 00:01:50 +0300

    замена всех цифр в скопированной строке на их названия

--- blockbuffer/blockbuffer.cpp
+++ blockbuffer/blockbuffer.cpp
@@ -6,18 +6,66 @@ int ClipBoardInpText(LPWSTR buf);
 
 int WINAPI wWinMain(HINSTANCE hIns, HINSTANCE hPrevIns, LPSTR pCmdLine, int nCmdShow)
 {
-    LPSTR Data;
+    LPWSTR Data;
     TCHAR Alert[] = L"Вы нарушили что-то, скопировав текст:";
-    TCHAR third[512];
+    TCHAR third[10240];
+    LPWSTR NewData = calloc(sizeof(WCHAR), 40960);
+    LPWSTR NnData = calloc(sizeof(WCHAR), 40960);
     while (TRUE)
     {
         Data = ClipBoardOutText();
         if (*Data != 0)
         {
-            swprintf(third, sizeof third, L"%s %s", Alert, Data);
+            
+            //*NewData = " ";
+            while (*Data != '\0')
+            {
+                switch (*Data)
+                {
+                case '0':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Ноль");
+                    break;
+                case '1':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Один");
+                    break;
+                case '2':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Два");
+                    break;
+                case '3':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Три");
+                    break;
+                case '4':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Четыре");
+                    break;
+                case '5':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Пять");
+                    break;
+                case '6':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Шесть");
+                    break;
+                case '7':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Семь");
+                    break;
+                case '8':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Восемь");
+                    break;
+                case '9':
+                    wsprintf(NnData, L"%s[%s]", NewData, L"Девять");
+                    break;
+                default:
+                    wsprintf(NnData, L"%s%c", NewData, *Data);
+                    break;
+                }
+                Data++;
+                NewData = NnData;
+            }
+
+            swprintf(third, sizeof third, L"%s %s", Alert, NewData);
             MessageBox(NULL, third, L"Внимание!", MB_OK);
             ClipBoardInpText("");
             Data = NULL;
+            *NewData = '\0';
+            *NnData = '\0';
         }
         Sleep(1000);
     }
